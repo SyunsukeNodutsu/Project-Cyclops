624f2c8e9bd429872df6f6ffc6bbea33c211ba24
SyunsukeNodutsu <nozuo0630@gmail.com>
2022-05-28 04:25:35 +0900

    EditorCamera.h/cpp追加
    ・EditorCamera.h/cpp追加

---
+++ Engine/Source/Editor/EditorCamera.h
@@ -0,0 +1,18 @@
+﻿//-----------------------------------------------------------------------------
+// File: EditorCamera.h
+//
+// 編集カメラ
+//-----------------------------------------------------------------------------
+#pragma once
+
+//編集カメラクラス
+class EditorCamera : public Camera
+{
+public:
+
+	EditorCamera();
+	~EditorCamera() = default;
+
+private:
+
+};
