5c2f5bc9493bb3266baeb3f60419aa87d6ea5144
SyunsukeNodutsu <nozuo0630@gmail.com>
2022-06-19 14:24:01 +0900

    モジュールの説明コメント追加
    ・モジュールの説明コメント追加

--- Engine/Source/Graphics/Particle/Module/CollisionModule.h
+++ Engine/Source/Graphics/Particle/Module/CollisionModule.h
@@ -1,11 +1,12 @@
 ﻿//-----------------------------------------------------------------------------
 // File: CollisionModule.h
 //
-// 
+// モジュールコリジョン 衝突の制御を設定
 //-----------------------------------------------------------------------------
 #pragma once
 #include "ParticleSystemModule.h"
 
+//コリジョンモジュールクラス
 class CollisionModule : public ParticleSystemModule
 {
 public:
--- Engine/Source/Graphics/Particle/Module/EmissionModule.h
+++ Engine/Source/Graphics/Particle/Module/EmissionModule.h
@@ -1,11 +1,12 @@
 ﻿//-----------------------------------------------------------------------------
 // File: EmissionModule.h
 //
-// 
+// モジュールエミッション 放射の頻度とタイミングを設定
 //-----------------------------------------------------------------------------
 #pragma once
 #include "ParticleSystemModule.h"
 
+//エミッションモジュールクラス
 class EmissionModule : public ParticleSystemModule
 {
 public:
